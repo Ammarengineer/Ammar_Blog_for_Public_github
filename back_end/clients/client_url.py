f2f78b9efd12b95ca9dc35240244612c16b86c32
Ammarengineer <124346188+Ammarengineer@users.noreply.github.com>
2023-07-14 18:54:59 +0300

    Update client_url.py

--- back_end/clients/client_url.py
+++ back_end/clients/client_url.py
@@ -1,4 +1,4 @@
 # all_blogs_url = "http://192.168.1.104:8000/"
 all_blogs_url = 'http://127.0.0.1:8000/'
-auth_url = 'http://192.168.1.104:8000/api/token/'
+auth_url = 'http://127.0.0.1:8000//api/token/'
 add_new_blog_url = f'{all_blogs_url}/make_new_blog/'
